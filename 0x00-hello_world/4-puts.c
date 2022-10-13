cc8ac8ed0f42e181eaa9a7e1e4b91aa27d983adf
Philisiwe <Philisiwedhlamini7@gmail.com>
2022-10-13 12:43:00 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /**
  * main -Entry
  * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilungual puzzle");
+	puts("/"Programming is like building a multilingual puzzel");
 	return (0);
 }
