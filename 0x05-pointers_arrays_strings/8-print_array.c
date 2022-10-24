f321aaa8b5a584a1a7cee9c606185645280dd699
Philisiwe <Philisiwedhlamini7@gmail.com>
2022-10-24 16:18:57 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints n element of an array of integers
+ * @a: int to check
+ * @n: number of values to be checked
+ */
+
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		printf("%d", a[t]);
+		if (t != n - 1)
+			printf(", ")
+	}
+	printf("\n");
+}
