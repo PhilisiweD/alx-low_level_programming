3e110b6502a019043350b0e5df1172744b3b0926
Philisiwe <Philisiwedhlamini7@gmail.com>
2022-11-16 06:26:04 -0800

    my first commit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,16 +18,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (ops[i].f != NULL)
+	while (i < 5)
 	{
-		if (*s == *(ops[i].op) && s[1] == '\0')
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+
 		i++;
 	}
 
-	printf("Error\n");
-	exit(99);
+	return (0);
 }
